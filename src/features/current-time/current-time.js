e715a52bbfd0512f4a37c7f8f6650449008b9ac2
PRISM-EXE <jwalker.swe@gmail.com>
2025-01-27 18:49:59 -0600

    Fixed clock, now updates automatically

--- src/features/current-time/current-time.js
+++ src/features/current-time/current-time.js
@@ -17,6 +17,14 @@ let divider = document.querySelector('.divider');
 
 
 const changeTime = function() {
+    let date = new Date();
+
+    let easternTimeHrs = date.getHours().toLocaleString("en-US", {timeZone: "America/New_York", timeZoneName: "short"});
+    let easternTimeMinutes = date.getMinutes().toLocaleString("en-US", {timeZone: "America/New_York", timeZoneName: "short"});
+
+    let currentHrs = easternTimeHrs;
+    let currentMins = easternTimeMinutes;
+
     if (easternTimeHrs > 12) {
         currentHrs = easternTimeHrs - 12;
         meridiem.innerHTML = 'PM';
@@ -41,14 +49,6 @@ const changeTime = function() {
     } else {
         minutes.innerHTML = currentMins;
     }
-
-    // let script = document.querySelector('.current-time');
-
-    // let timestamp = date.getTime().toLocaleString("en-US", {timeZone: "America/New_York", timeZoneName: "short"} );
-
-    // let scriptUrl = "./src/features/current-time/current-time.js?v=" + timestamp;
-
-    // script.src = scriptUrl;
 
     return
 }
